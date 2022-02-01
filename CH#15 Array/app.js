f74175dee780090300b0f5fb05c8c77e16bc7372
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-01 10:55:42 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#15 Array/app.js
@@ -0,0 +1,11 @@
+// simply Variable
+var city1="multan";
+var city2="lahore";
+var city3="karachi";
+var city4="pindi";
+
+// Array declration
+var city=["multan","lahore","karachi","Islamabad"];
+//index start from 0
+
+alert("Welcome to : "+ city[3]);  //islamabad
