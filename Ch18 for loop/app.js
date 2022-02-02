65de7c8fc810eb9e5197297c724007e0dbe9a976
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-02 21:38:22 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ Ch18 for loop/app.js
@@ -0,0 +1,21 @@
+// for(var i=1;i<=10;i++)
+// {
+//     document.write(i+ "<br>");
+// }
+// Adding value in Array
+
+// var arr=[];
+// for(var i=0;i<=10;i++)
+// {
+//    arr[i]=i+2;
+// }
+// console.log(arr);
+var arr=["multan","lahore","Quetta"];
+for(var i=0;i<arr.length;i++)
+{
+   if(arr[i]==="multan")
+   {
+       alert("multan is cleanest city");
+   }
+}
+console.log(arr);
