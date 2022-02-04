79e5ec239367054c38d01013e2fdb799627d585a
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-04 18:28:13 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#26 Math Round/app.js
@@ -0,0 +1,13 @@
+var roundNumber=4.58654;
+roundNumber=Math.round(roundNumber);
+roundNumber=Math.ceil(roundNumber);
+roundNumber=Math.floor(roundNumber);
+console.log(roundNumber);
+
+/*
+Math.round() it convert to 1.5 into 2 and 1.4 into 1;
+Math.floor() it convert to 1.5 into 1, it fource full downgrade the number;
+Math.ceil() it convert to 1.5 into 2, it fource full upgrade the number;
+
+*/
+
