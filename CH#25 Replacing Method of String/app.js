c34f0b2bb0af79413a043b843029955fd114676c
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-04 18:09:27 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#25 Replacing Method of String/app.js
@@ -0,0 +1,5 @@
+var sentence="It is a long established  be distracted by the readable content of a page when looking at its layout long. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years,long sometimes by accident, sometimes on purpose (injected humour and the like). ";
+var changeResult=sentence.replace(/long/g,"Farhan");
+// var changeResult=sentence.replace("layout","Farhan");
+console.log(changeResult);
+
