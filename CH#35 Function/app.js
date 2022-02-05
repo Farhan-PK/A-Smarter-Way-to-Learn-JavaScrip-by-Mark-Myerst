06ec4eae3a00b28dee9bf1d167cc48574a9e5821
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-06 03:08:00 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#35 Function/app.js
@@ -0,0 +1,18 @@
+// function Call()
+// {
+//     alert("Hello Farhan Bashir");
+// }
+
+// Passing Para Meter
+
+// function Call(num1,num2)
+// {
+//     alert(num1+num2);
+// }
+
+// Return Value
+function Call(num1,num2)
+{
+    var sum=num1+num2;
+    return sum;
+}
