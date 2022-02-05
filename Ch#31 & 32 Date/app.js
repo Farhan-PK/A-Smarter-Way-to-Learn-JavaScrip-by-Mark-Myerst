3cdef1a0d9dd63ced0a2d6528ff13b9d33ae8dd1
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-05 22:51:04 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ Ch#31 & 32 Date/app.js
@@ -0,0 +1,15 @@
+var todayDate=new Date();
+console.log(todayDate);
+var today=todayDate.getDay();
+console.log(today);
+var days=["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+console.log(days[today]);
+console.log(todayDate.getDate());
+console.log(todayDate.getMonth());
+console.log(todayDate.getFullYear());
+console.log(todayDate.getHours());
+console.log(todayDate.getMinutes());
+console.log(todayDate.getMilliseconds());
+console.log(todayDate.getTime());
+
+
