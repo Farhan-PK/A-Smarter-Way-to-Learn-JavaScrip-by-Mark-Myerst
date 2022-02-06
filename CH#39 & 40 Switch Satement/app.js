36977e300508e55cc97779e03b3194a9f4e22baa
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-06 12:43:29 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#39 & 40 Switch Satement/app.js
@@ -0,0 +1,27 @@
+var day = +prompt("Enter Your day Number:", "1....7");
+switch (day) {
+  case 1:
+    alert("Hello Sunday!");
+    break;
+  case 2:
+    alert("Hello Monday");
+    break;
+  case 3:
+    alert("Hello TuesDay!");
+    break;
+  case 4:
+    alert("Hello Wednesday");
+    break;
+  case 5:
+    alert("Hello Thursday");
+    break;
+  case 6:
+    alert("Hello Friday!");
+    break;
+  case 7:
+    alert("Hello Saturday");
+    break;
+  default:
+    alert("Select right Number");
+    break;
+}
