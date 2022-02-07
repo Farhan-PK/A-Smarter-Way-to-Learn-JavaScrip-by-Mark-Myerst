7d7ba9b10610efa8feaf1a629a7cd5251d37c2f4
Farhan-PK <farhanwebsolution@gmail.com>
2022-02-08 02:17:48 +0500

    A-Smarter-Way-to-Learn-JavaScrip-by-Mark-Myerst

---
+++ CH#56&57 Target all elements by tag name/app.js
@@ -0,0 +1,8 @@
+var para=document.getElementsByTagName("p");
+para[0].style.fontSize="36px";
+var para=document.getElementsByTagName("p");
+para[1].style.color="green";
+var para=document.getElementsByTagName("p");
+para[2].style.fontWeight="bolder";
+var para=document.getElementsByTagName("p");
+para[3].style.textTransform="uppercase";
